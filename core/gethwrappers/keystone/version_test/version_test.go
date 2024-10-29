116c9b7094fd288ad4e83fde48afaa45051565ea
krehermann <16602512+krehermann@users.noreply.github.com>
2024-10-29 09:57:51 -0600

    add froozen keystone contract wrappers

---
+++ core/gethwrappers/keystone/version_test/version_test.go
@@ -0,0 +1,50 @@
+package versiontest
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/stretchr/testify/require"
+
+	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/assets"
+	kcr_1_0_1 "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/keystone/generated/capabilities_registry_1_0_1"
+	forwarder_1_0_0 "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/keystone/generated/forwarder"
+	ocr3_1_0_0 "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/keystone/generated/ocr3_capability_1_0_0"
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
+)
+
+func TestVersion(t *testing.T) {
+	owner := testutils.MustNewSimTransactor(t)
+	chain := backends.NewSimulatedBackend(core.GenesisAlloc{
+		owner.From: {Balance: assets.Ether(100).ToInt()},
+	}, 30e6)
+
+	t.Run("capabilities registry v1_0_1", func(t *testing.T) {
+		_, _, capreg_1_0_1, err := kcr_1_0_1.DeployCapabilitiesRegistry(owner, chain)
+		require.NoError(t, err)
+		chain.Commit()
+		tv, err := capreg_1_0_1.TypeAndVersion(nil)
+		require.NoError(t, err)
+		require.Equal(t, "CapabilitiesRegistry 1.0.1", tv)
+	})
+
+	t.Run("ocr3 capability v1_0_0", func(t *testing.T) {
+		_, _, ocr3_1_0_0, err := ocr3_1_0_0.DeployOCR3Capability(owner, chain)
+		require.NoError(t, err)
+		chain.Commit()
+		tv, err := ocr3_1_0_0.TypeAndVersion(nil)
+		require.NoError(t, err)
+		require.Equal(t, "Keystone 1.0.0", tv)
+	})
+
+	t.Run("forwarder v1_0_0", func(t *testing.T) {
+		_, _, forwarder, err := forwarder_1_0_0.DeployKeystoneForwarder(owner, chain)
+		require.NoError(t, err)
+		chain.Commit()
+		tv, err := forwarder.TypeAndVersion(nil)
+		require.NoError(t, err)
+		require.Equal(t, "Forwarder and Router 1.0.0", tv)
+	})
+
+}
